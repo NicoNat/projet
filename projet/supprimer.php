f92586d4202e1c2824e73b908b937cfd656e417e
NicoNat <37415698+NicoNat@users.noreply.github.com>
2018-03-27 09:59:41 +0200

    Create supprimer.php

---
+++ projet/supprimer.php
@@ -0,0 +1,50 @@
+<?php
+require_once('fonction.php');
+require_once('utilisateur.php');
+$id_bdd = Id_bdd();
+
+try
+{
+	$bdd = new PDO($id_bdd['nsd'],$id_bdd['id'],$id_bdd['mdp']);
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+
+$_SESSION['question'] = 1;
+$i = 0;
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>BDD</title>
+    </head>
+
+    <body>
+		
+		<?php
+		
+		if($_SESSION['prof'] == 1)
+		{
+			
+			$resultat = $bdd -> query("DELETE FROM QUESTIONNAIRE WHERE id = {$_GET["id"]}");
+			if($resultat == FALSE){
+				die("Erreur : suppression non effectuée");
+			}
+			else {
+				echo "Suppression effectuée <br />";
+			}
+			
+			echo "<br />";
+			echo '<a href="choix.php">Accueil</a>'
+			
+		}
+		
+		?>
+		
+    </body>
+</html>
