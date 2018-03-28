cdc5b5cea59601e1554eabc157e258a3915f9bd8
NicoNat <37415698+NicoNat@users.noreply.github.com>
2018-03-28 10:23:23 +0200

    Update supprimer.php

--- projet/supprimer.php
+++ projet/supprimer.php
@@ -30,7 +30,7 @@ $i = 0;
 		
 		if($_SESSION['prof'] == 1)
 		{
-			$resultat = $bdd -> query("DELETE FROM QUESTIONNAIRE WHERE id =".$_GET['id_questionnaire']);
+			$resultat = $bdd -> query("DELETE FROM QUESTION WHERE id_questionnaire IN (SELECT id_questionnaire FROM QUESTIONNAIRE WHERE id_questionnaire=".$_GET['id_questionnaire']."); DELETE FROM QUESTIONNAIRE WHERE id_questionnaire =".$_GET['id_questionnaire']);
 			if($resultat == FALSE){
 				die("Erreur : suppression non effectuée");
 			}
